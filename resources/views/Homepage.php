d2b9eed565cf796b897c3ddae6d4badef7b78777
Toms <tomy22783@gmail.com>
2020-07-16 22:03:42 +0300

    :art: Added cdn tailwind

--- resources/views/Homepage.php
+++ resources/views/Homepage.php
@@ -1,1 +1,12 @@
-<h1>This is the homepage</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-5xl">This is the homepage</h1>
+</body>
+</html>
--- resources/views/task/TaskEdit.php
+++ resources/views/task/TaskEdit.php
@@ -1,1 +1,12 @@
-<h1>Edit task</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit a task</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-5xl">Edit a task</h1>
+</body>
+</html>
