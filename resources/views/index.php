196418bbc77129cc8170f1b0d92966848b33290b
Toms <tomy22783@gmail.com>
2020-07-17 20:47:24 +0300

    :art: Added TailwindCSS, TailwindCSS Custom Forms

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,16 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    >
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge"
-    >
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/resources/css/style.css">
     <title>Document</title>
 </head>
 <body
-<div>
+<div class="text-red-600">
     <table>
         <thead>
         <tr>
